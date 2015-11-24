c772d15398791097b45c31f5a5dff146ef98de7d
Vivek Patani <vivek.patani@somaiya.edu>
2015-11-24 02:09:19 -0500

    Doubly Linked List Update: Added addAfter and comments.

--- src/com/vivekpatani/linkedList/DoublyLinkedList.java
+++ src/com/vivekpatani/linkedList/DoublyLinkedList.java
@@ -6,12 +6,16 @@
 package com.vivekpatani.linkedList;
 
 /**
- * 
+ * To implement a simple DoublyLinkedList
  */
 public class DoublyLinkedList {
 
 	private static NodeLinkedList head, tail, currentNode;
 	
+	/**
+	 * The driver method used to run the class
+	 * @param args
+	 */
 	public static void main(String[] args){
 		DoublyLinkedList dLL = new DoublyLinkedList();
 		
@@ -19,8 +23,9 @@ public class DoublyLinkedList {
 		dLL.addAtEnd(1);
 		dLL.addAtEnd(2);
 		dLL.addAtEnd(3);
+		dLL.addAfter(1,2);
 		traverseForward();
-		removeElement(2);
+		removeElement(3);
 		traverseBackward();
 	}
 	
@@ -29,21 +34,81 @@ public class DoublyLinkedList {
 		tail = null;
 	}
 	
+	/**
+	 * This is use to just assign the header when the LinkedList is empty.
+	 * @param newNode
+	 */
+	public static void setHead(NodeLinkedList newNode){
+		head = newNode;
+		tail = newNode;
+	}
+	
+	/**
+	 * Method used to add a node at the start of the LinkedList
+	 * @param value
+	 */
+	public static void addAtStart(int value){
+		NodeLinkedList newNode = new NodeLinkedList(value);
+		
+		if(isEmpty()){
+			setHead(newNode);
+		} else {
+			head.setPrevRef(newNode);
+			head.setNextRef(head);
+			head = newNode;
+			head.setPrevRef(null);
+		}
+	}
+	
+	/**
+	 * Method used to add a node at the end of the LinkedList
+	 * @param value
+	 */
 	public void addAtEnd (int value) {
 		NodeLinkedList newNode = new NodeLinkedList(value);
 		
-		if (head == null){
+		if (isEmpty()){
 			head = newNode;
 			tail = newNode;
 		} else {
 			tail.setNextRef(newNode);
 			newNode.setPrevRef(tail);
 			tail = newNode;
-			tail.setNextRef(null);
 		}
-		
 	}
 	
+	/**
+	 * This is used to add a Node after a certain value.
+	 * @param value
+	 */
+	public void addAfter(int value, int index){
+		NodeLinkedList newNode = new NodeLinkedList(value);
+		currentNode = head;
+		
+		if(isEmpty()){
+			setHead(newNode);
+		} else {
+			do {
+				currentNode = currentNode.getNextRef();
+			} while (currentNode!=null && currentNode.getValue()!= index);
+			
+			if(currentNode == null){
+				System.out.println(ConstantsLinkedList.LinkedListConstants[2]);
+			} else if (currentNode == tail){
+				addAtEnd(value);
+			} else {
+				currentNode.getNextRef().setPrevRef(newNode);
+				newNode.setNextRef(currentNode.getNextRef());
+				currentNode.setNextRef(newNode);
+				newNode.setPrevRef(currentNode);
+				
+			}
+		}
+	}
+	
+	/**
+	 * Method used to the LinkedList in a reverse manner
+	 */
 	public static void traverseBackward () {
 		
 		System.out.println("\n"+ConstantsLinkedList.LinkedListConstants[1]);
@@ -54,6 +119,9 @@ public class DoublyLinkedList {
 		}
 	}
 	
+	/**
+	 * Method used to the LinkedList in a forward manner
+	 */
 	public static void traverseForward (){
 		
 		System.out.println(ConstantsLinkedList.LinkedListConstants[0]);
@@ -64,6 +132,10 @@ public class DoublyLinkedList {
 		}
 	}
 	
+	/**
+	 * Method used to remove an element from the Doubly LinkedList
+	 * @param value
+	 */
 	public static void removeElement(int value) {
 		
 		currentNode = head;
@@ -86,4 +158,14 @@ public class DoublyLinkedList {
 			System.out.println(ConstantsLinkedList.LinkedListConstants[3]);
 		}
 	}
+	
+	/**
+	 * Checks whether if the LinkedList is empty or not
+	 * The idea is to check whether if head is null or not
+	 */
+	private static boolean isEmpty() {
+		if(head == null) return true;
+		return false;
+	}
+	
 }
