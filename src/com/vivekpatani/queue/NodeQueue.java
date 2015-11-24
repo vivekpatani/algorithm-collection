6ea5dc855b0ec13977f36b4f391fc8e8046d9891
Vivek Patani <vivek.patani@somaiya.edu>
2015-11-24 18:08:26 -0500

    Queue Data Structure, Priority Queue is not working. Need to check.

--- src/com/vivekpatani/queue/NodeQueue.java
+++ src/com/vivekpatani/queue/NodeQueue.java
@@ -11,21 +11,33 @@ package com.vivekpatani.queue;
 public class NodeQueue {
 	
 	private NodeQueue next;
-	private int value;
+	private int value, priority;
 	
 	/**
 	 * Basic Constructor
 	 */
 	public NodeQueue(){
 		next = null;
+		priority = 0;
 	}
 	
 	/**
-	 * Basic Constructor with value initiate
+	 * Basic Constructor with value
 	 * @param value
 	 */
 	public NodeQueue(int value){
 		this.value = value;
+		priority = 0; //This is used for normal Queue, hence all Nodes are of same priority.
+	}
+	
+	/**
+	 * Basic Constructor with value and priority
+	 * @param value
+	 * @param priority
+	 */
+	public NodeQueue(int value, int priority){
+		this.value = value;
+		this.priority = priority;
 	}
 	
 	/**
@@ -36,6 +48,22 @@ public class NodeQueue {
 		return next;
 	}
 	
+	/**
+	 * Method used to get the priority of the Node
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * Method used to set the priority of the Node
+	 * @param priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
 	/**
 	 * Method used to return the value
 	 * @return the value
