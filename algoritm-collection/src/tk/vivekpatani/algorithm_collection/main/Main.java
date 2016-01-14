e875a6c9f92014b8365de8cb90aff079b6855e35
Vivek <vivek.patani@somaiya.edu>
2016-01-14 16:25:26 -0500

    Thr initial commit

---
+++ algoritm-collection/src/tk/vivekpatani/algorithm_collection/main/Main.java
@@ -0,0 +1,47 @@
+/**
+ * Vivek Patani {FlipSwitch}
+ * Main.java
+ * {Algorithms 0.: Living in Beta}
+ */
+package tk.vivekpatani.algorithm_collection.main;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// This is the welcome message.
+		System.out.println("=====================================================================");
+		System.out.println("||                                                                 ||");
+		System.out.println("||                                                                 ||");
+		System.out.println("||                        Welcome to this side!                    ||");
+		System.out.println("||                                                                 ||");
+		System.out.println("||                                                                 ||");
+		System.out.println("=====================================================================");
+		
+		//The code begins here
+		
+		optionChooser();
+		
+		// Ciao Message.
+		System.out.println("=====================================================================");
+		System.out.println("||                                                                 ||");
+		System.out.println("||                                                                 ||");
+		System.out.println("||                        See you soon, buddy!!                    ||");
+		System.out.println("||                                                                 ||");
+		System.out.println("||                                                                 ||");
+		System.out.println("=====================================================================");
+	}
+	
+	private static void optionChooser(){
+		
+		System.out.println("Choose your option, choose it wisely");
+		System.out.println("1. Implementing a Stack Data Structure");
+	}
+
+}
