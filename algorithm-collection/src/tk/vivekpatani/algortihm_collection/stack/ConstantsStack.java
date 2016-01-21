04e136b30b997000f2a5d4c0b8edea7f6fb55810
Vivek <vivek.patani@somaiya.edu>
2016-01-21 17:00:51 -0500

    Various Changes
    Various Changes

---
+++ algorithm-collection/src/tk/vivekpatani/algortihm_collection/stack/ConstantsStack.java
@@ -0,0 +1,16 @@
+/**
+ * Vivek Patani {FlipSwitch}
+ * ConstantsStack.java
+ * {Algorithms 0.: Living in Beta}
+ */
+package tk.vivekpatani.algortihm_collection.stack;
+
+/**
+ * 
+ */
+public class ConstantsStack {
+	
+	public static final String[] OPTIONS_STACK = new String[] {"1 Simple Stack Demonstration (Integer)","2. Simple Stack Demonstration (String)", "3 Simple Stack Demonstration (Character)", "4. Reverse a String", "5. Convert Number to Binary"};
+	public static final String[] OPTIONS_STACK_TYPE = new String[] {"Input Integers", "Input String", "Input Character", "Input Object"};
+	public static final String[] OPTIONS_STACK_INT = new String[] {"1. Add Elements, 2. Push Element", "3.Pop Element", "4. Peek", "5. Multipop", "6. Destroy", "7. Exit"};
+}
