2d8dee434fe34cf8af9c8a95037f2b5f74be2a43
Vivek <vivek.patani@somaiya.edu>
2016-03-03 20:35:14 -0500

    Adding Travis CI

--- src/tk/vivekpatani/algorithm_collection/misc/ConvertDecimal.java
+++ src/tk/vivekpatani/algorithm_collection/misc/ConvertDecimal.java
@@ -14,7 +14,6 @@ public class ConvertDecimal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Result: "+convertToBinary(128));
 		System.out.println("Result: "+convertToOctal(128));
 	}
