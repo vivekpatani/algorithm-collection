f128ab8d0e1b8cc171dc5458cb9fcb874f1079d6
Vivek Patani <vivek.patani@somaiya.edu>
2016-08-11 13:53:28 -0400

    Added First Test

---
+++ test/common/ConstantsCommonTest.java
@@ -0,0 +1,15 @@
+package common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConstantsCommonTest {
+
+	@Test
+	public void test() {
+		
+		assertEquals(0, 0);
+	}
+
+}
