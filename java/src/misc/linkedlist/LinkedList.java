95f53b6014557694d39ba0e4ce96dc844d06e08b
Vivek Patani <vivek.patani@somaiya.edu>
2016-08-10 20:18:05 -0400

    Refactoring and LinkedList Init

---
+++ java/src/misc/linkedlist/LinkedList.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class LinkedList {
+
+	private int size = -1;
+
+	public LinkedList() {
+		List linkedList = new ArrayList();
+		size++;
+	}
+
+	public LinkedList(int firstNumber) {
+		List linkedList = new ArrayList();
+		size++;
+
+	}
+
+	public static void main(String[] args) {
+
+
+	}
+}
