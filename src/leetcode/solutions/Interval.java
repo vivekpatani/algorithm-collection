23da9faf74a9f34e82566020d755ea1db4622d8c
Vivek Patani <vivek.patani@somaiya.edu>
2016-11-17 02:09:10 -0500

    Working Leetcode #11

---
+++ src/leetcode/solutions/Interval.java
@@ -0,0 +1,30 @@
+package leetcode.solutions;
+
+/**
+ * Class Interval
+ * @author flipswitch
+ *
+ */
+public class Interval {
+	
+	int start;
+	int end;
+
+	/**
+	 * Constructor
+	 */
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	/**
+	 * Constructor
+	 * @param s
+	 * @param e
+	 */
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
